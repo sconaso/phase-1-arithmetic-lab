f136c769592b101f2e6d1bc6e9bc84abf0311415
Scott Nason <scottmnason@gmail.com>
2021-12-04 22:27:29 -0700

    commit tree 7be4eea66f0c1393a73b81587bf4e2586b11c1d3

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10)
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+let max = (Math.max(5, 10, 15, 20));
